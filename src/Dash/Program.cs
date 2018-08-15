8a2c667a3fd6cac0f25ccd7e85f494e1384e58e5
Thomas Fischer <thfis@dfds.com>
2018-08-15 11:23:23 +0200

    add solution and webapi and test projects

---
+++ src/Dash/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Dash
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args)
+                .Build()
+                .Run();
+        }
+
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost
+                .CreateDefaultBuilder(args)
+                //.ConfigureServices(x => x.AddTransient())
+                .UseStartup<Startup>();
+        }
+    }
+}
---
+++ src/Dash/Startup.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NJsonSchema;
+using NSwag;
+using NSwag.AspNetCore;
+using NSwag.SwaggerGeneration.Processors.Security;
+
+namespace Dash
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                //app.UseHsts();
+            }
+
+            app.UseSwaggerDocumentation(env);
+
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+
+    public static class SwaggerDocumentationExtensions
+    {
+        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseSwaggerUi(typeof(Startup).Assembly, settings =>
+            {
+                settings.PostProcess = document =>
+                {
+                    document.Info.Description = "Seabook Terms and Conditions API V1";
+                    document.Security.Add(new SwaggerSecurityRequirement {{"Bearer", new string[] { }}});
+                    document.Schemes = new List<SwaggerSchema> {env.IsDevelopment() ? SwaggerSchema.Http : SwaggerSchema.Https};
+                };
+
+                settings.GeneratorSettings.DefaultPropertyNameHandling = PropertyNameHandling.CamelCase;
+                settings.GeneratorSettings.Title = "Seabook Terms and Conditions API Swagger documentation";
+
+                settings.GeneratorSettings.DocumentProcessors.Add(new SecurityDefinitionAppender("Bearer",
+                    new SwaggerSecurityScheme
+                    {
+                        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                        Type = SwaggerSecuritySchemeType.ApiKey,
+                        Name = "X-Api-Key",
+                        In = SwaggerSecurityApiKeyLocation.Header
+                    }));
+            });
+
+            return app;
+        }
+    }
+}
