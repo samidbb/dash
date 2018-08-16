8c2e4566eaa64b4d4ca4223f671ec17f8e60b85e
Thomas Fischer <thfis@dfds.com>
2018-08-16 12:56:07 +0200

    add id generator

---
+++ src/Dash.Tests/TestIdGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using Dash.Infrastructure;
+using Xunit;
+
+namespace Dash.Tests
+{
+    public class TestIdGenerator
+    {
+        [Theory]
+        [InlineData("aws-account-billing.json")]
+        [InlineData("aws-billing.json")]
+        [InlineData("aws-elasticsearch.json")]
+        [InlineData("aws-s3.json")]
+        [InlineData("blaster-red.json")]
+        [InlineData("grafana.json")]
+        [InlineData("kubernetes-capacity-planning.json")]
+        [InlineData("kubernetes-cluster-health-overview.json")]
+        [InlineData("kubernetes-deployment.json")]
+        [InlineData("kubernetes-fluentd-pods.json")]
+        [InlineData("kubernetes-general-overview.json")]
+        [InlineData("kubernetes-kube2iam.json")]
+        [InlineData("kubernetes-monitoring-setup.json")]
+        [InlineData("kubernetes-nodes.json")]
+        [InlineData("kubernetes-persistent-volumes.json")]
+        [InlineData("kubernetes-pods.json")]
+        [InlineData("kubernetes-resource-requests.json")]
+        [InlineData("kubernetes-statefulset.json")]
+        [InlineData("kubernetes-tiller.json")]
+        [InlineData("kubernetes-weave-net.json")]
+        [InlineData("tbd20180803-traefik-red-metrics.json")]
+        [InlineData("traefik-detailed.json")]
+        [InlineData("wikijs.json")]
+        public void Id_only_has_valid_characters(string name)
+        {
+            var id = IdGenerator.Generate(name);
+
+            Assert.True(id.All(char.IsLetterOrDigit));
+        }
+    }
+}
---
+++ src/Dash/Infrastructure/IdGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Dash.Infrastructure
+{
+    public static class IdGenerator
+    {
+        public static string Generate(string input)
+        {
+            using (var sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                return string.Concat(Convert.ToBase64String(hash).ToCharArray().Where(char.IsLetterOrDigit).Take(9));
+            }
+        }
+    }
+}
