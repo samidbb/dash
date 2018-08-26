3cd39bfa84830bc109014100e5eae60837e1f124
Thomas Wittrup Fischer <twf@trifork.com>
2018-08-26 22:38:24 +0200

    WIP: cleanup unit tests

--- src/Dash.Tests/Domain/A.cs
+++ src/Dash.Tests/Domain/A.cs
@@ -1,4 +1,7 @@
 ﻿using Dash.Domain;
+using Dash.Infrastructure;
+using Dash.Infrastructure.Configuration;
+using Dash.Infrastructure.Versioning;
 using Dash.Tests.Infrastructure.Configuration;
 
 namespace Dash.Tests.Domain
@@ -7,5 +10,43 @@ namespace Dash.Tests.Domain
     {
         public static DashboardBuilder Dashboard => new DashboardBuilder();
         public static FakeFileBuilder File => new FakeFileBuilder();
+        public static DashboardServiceBuilder DashboardService => new DashboardServiceBuilder();
+        public static DashboardConfigurationRepositoryBuilder DashboardConfigurationRepository => new DashboardConfigurationRepositoryBuilder();
     }
+    
+    public class DashboardServiceBuilder
+    {
+        private FileVersionRepository _fileVersionRepository;
+        private DashboardConfigurationRepository _dashboardConfigurationRepository;
+        private FileSystem _fileSystem;
+
+        public DashboardServiceBuilder With(FileVersionRepository fileVersionRepository)
+        {
+            _fileVersionRepository = fileVersionRepository;
+            return this;
+        }
+
+        public DashboardServiceBuilder With(DashboardConfigurationRepository dashboardConfigurationRepository)
+        {
+            _dashboardConfigurationRepository = dashboardConfigurationRepository;
+            return this;
+        }
+
+        public DashboardServiceBuilder With(FileSystem fileSystem)
+        {
+            _fileSystem = fileSystem;
+            return this;
+        }
+
+        public DashboardService Build()
+        {
+            return new DashboardService(_fileVersionRepository, _dashboardConfigurationRepository, _fileSystem);
+        }
+
+        public static implicit operator DashboardService(DashboardServiceBuilder builder)
+        {
+            return builder.Build();
+        }
+    }
+
 }
